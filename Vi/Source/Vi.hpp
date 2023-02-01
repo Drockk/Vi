a802d2b05b9c6e6400d0f4cdb82d2de3c67b48a1
Bartosz Zielonka <batziell@gmail.com>
2023-02-01 20:57:47 +0100

    repo cleanup

--- Vi/Source/Vi.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-//For Vi Application clients
-#include "Vi/Core/Application.hpp"
-#include "Vi/Core/Layer.hpp"
--- Vi/Source/vipch.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <functional>
-#include <memory>
-#include <string>
-
-#include "Vi/Core/Assert.hpp"
-#include "Vi/Core/Log.hpp"
-#include "Vi/Core/ShutdownQueue.hpp"
