f078e30bb654d1492775479c021fe62e91e0542e
Bartosz Zielonka <batziell@gmail.com>
2023-02-09 21:45:01 +0100

    Cleanup repo

--- Vi/source/vk_init.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "vk_types.hpp"
-
-namespace Vkinit {
-    VkCommandPoolCreateInfo commandPoolCreateInfo(uint32_t queueFamilyIndex, VkCommandPoolCreateFlags flags = 0);
-
-    VkCommandBufferAllocateInfo commandBufferAllocateInfo(VkCommandPool pool, uint32_t count = 1, VkCommandBufferLevel level = VK_COMMAND_BUFFER_LEVEL_PRIMARY);
-}
